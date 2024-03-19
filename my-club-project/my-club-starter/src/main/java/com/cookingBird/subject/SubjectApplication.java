660ea9e651b21eadc827c87f80e949e606fc28f0
cooking_bird <d17381844023@gmail.com>
2024-03-20 01:42:00 +0800

    page 1

--- my-club-project/my-club-starter/src/main/java/com/cookingBird/subject/SubjectApplication.java
+++ my-club-project/my-club-starter/src/main/java/com/cookingBird/subject/SubjectApplication.java
@@ -1,11 +1,13 @@
 package com.cookingBird.subject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.cookingBird")
+@MapperScan("com.cookingBird.**.mapper")
 public class SubjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubjectApplication.class);
